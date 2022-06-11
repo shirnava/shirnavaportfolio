73c56a376accd3b698fb17a11b945c142c546b86
shirnava <shirnava@gmail.com>
2022-06-11 16:56:24 -0400

    add initial code structures and references to sections in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Work from "./components/work/Work";
+import Projects from "./components/projects/Projects";
+import Contact from "./components/contact/Contact";
 
 function App() {
   return (
     <div className="app">
-      top
-      sec -intro -portfolio - work
+      <Topbar/>
+
+      {/* inside this div, will call all section components */}
+      <div className="sections">
+      <Intro/>
+      <Portfolio/>
+      <Work/>
+      <Projects/>
+      <Contact/>
+      </div>
     </div>
   );
 }

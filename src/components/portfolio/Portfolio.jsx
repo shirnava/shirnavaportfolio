1e5bac752b045419fc75e38ff13186a1b470c034
shirnava <shirnava@gmail.com>
2022-08-18 22:09:48 -0400

    start portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,27 @@ import "./portfolio.scss"
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">
+          Featured
+        </li>
+        <li>
+          Discord Bot
+        </li>
+        <li>
+          Video Game
+        </li>
+        <li>
+          Machine Learning
+        </li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="" alt="" />
+          <h3></h3>
+        </div>
+      </div>
         
     </div>
   )

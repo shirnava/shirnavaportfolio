f6e7c371adadcbd07e3221546627f444bc324273
shirnava <shirnava@gmail.com>
2022-08-25 13:46:44 -0400

    added placeholder data for projects

--- src/data.js
+++ src/data.js
@@ -1,5 +1,76 @@
 export const featuredPortfolio = [
     {
-        
+        id: "1",
+        title: "Kpop Discord Card Bot",
+        img: "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002487/img/basic/a0002487_main.jpg?20220602111700&q=80&rw=750&rh=536"
+    },
+    {
+        id: "2",
+        title: "RE-RASSOR Cart FSI NASA",
+        img: "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002487/img/basic/a0002487_main.jpg?20220602111700&q=80&rw=750&rh=536"
+    },
+    {
+        id: "3",
+        title: "React Portfolio Website",
+        img: "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002487/img/basic/a0002487_main.jpg?20220602111700&q=80&rw=750&rh=536"
+    },
+    {
+        id: "4",
+        title: "Disharmonic Unity Game",
+        img: "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002487/img/basic/a0002487_main.jpg?20220602111700&q=80&rw=750&rh=536"
+    }
+]
+
+export const discordPortfolio = [
+    {
+        id: "1",
+        title: "Kpop Discord Card Bot",
+        img: "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002487/img/basic/a0002487_main.jpg?20220602111700&q=80&rw=750&rh=536"
+    }
+]
+export const videogamePortfolio = [
+    {
+        id: "1",
+        title: "Disharmonic Unity Game",
+        img: "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002487/img/basic/a0002487_main.jpg?20220602111700&q=80&rw=750&rh=536"
+    },
+    {
+        id: "2",
+        title: "The Legend of Kaneko Ono",
+        img: "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002487/img/basic/a0002487_main.jpg?20220602111700&q=80&rw=750&rh=536"
+    },
+    {
+        id: "2",
+        title: "Tic Tac Toetle",
+        img: "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002487/img/basic/a0002487_main.jpg?20220602111700&q=80&rw=750&rh=536"
+    }
+]
+
+export const mlPortfolio = [
+    {
+        id: "1",
+        title: "Predict future COVID-19 deaths",
+        img: "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002487/img/basic/a0002487_main.jpg?20220602111700&q=80&rw=750&rh=536"
+    },
+    {
+        id: "2",
+        title: "Predict Clothing Item",
+        img: "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002487/img/basic/a0002487_main.jpg?20220602111700&q=80&rw=750&rh=536"
+    }
+]
+
+
+export const webappPortfolio = [
+    {
+        id: "1",
+        title: "React Portfolio Website",
+        img: "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002487/img/basic/a0002487_main.jpg?20220602111700&q=80&rw=750&rh=536"
+    }
+]
+export const mobileappPortfolio = [
+    {
+        id: "1",
+        title: "RE-RASSOR Cart FSI NASA",
+        img: "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002487/img/basic/a0002487_main.jpg?20220602111700&q=80&rw=750&rh=536"
     }
 ]
